5930659c4d3696aa83ee4504fdf0d575a840bb27
Manz2 <erikmanzer21@gmail.com>
2024-01-09 08:51:17 +0100

    bootstrap working

--- frontend/MadnVue/src/main.js
+++ frontend/MadnVue/src/main.js
@@ -4,9 +4,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './registerServiceWorker'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 const app = createApp(App)
 
 app.use(router)
-
+// Make BootstrapVue available throughout your project
 app.mount('#app')
